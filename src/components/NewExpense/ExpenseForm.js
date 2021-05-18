34cbf1248f014dda118948ab2061711b642a96e2
DaviD D <jdavid.martinez@gmail.com>
2021-05-18 07:45:57 -0500

    VDO53 adding fuction amoun and date ChangeHandler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,30 +1,44 @@
+import { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const titleChangeHandler =(event) =>{
-      console.log(event.target.value) 
+  const [enteredTitle, setEnteredTilte] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTilte(event.target.value);
   };
+
+  const amountChangeHandler = (event) =>{
+      const [enteredAmount, setEnteredAmount] = useState("");
+      setEnteredAmount(event.target.value);
+  }
+
+  const dateChangeHandler = event => {
+      const [enteredDate, setEnteredDate] = useState("");
+      setEnteredDate(event.target.value)
+  }
 
   return (
     <form>
-    <div className="new-expense__controls">
-    <div className="new-expense__control">
-            <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-            <label>Amount</label>
-            <input type='number' min='0.01' step='0.01' />
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
-            <label>Date</label>
-            <input type='date' min="2019-01-01" max="2022-12-31"/>
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
-    </div>
-    <div className="new-expense__actions">
+      </div>
+      <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
-    </div>
-</form>
+      </div>
+    </form>
   );
 };
 export default ExpenseForm;
