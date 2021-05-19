6af61d56994455535c37fba2cc62508a1191b1ed
DaviD D <jdavid.martinez@gmail.com>
2021-05-18 21:25:19 -0500

    VDO57- setting inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,6 +47,9 @@ const ExpenseForm = () => {
     }
 
     console.log(expenseData);
+    setEnteredTilte('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -54,7 +57,10 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input 
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -62,6 +68,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -71,6 +78,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
